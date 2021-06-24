d7cb63b80d2a4a19b223ab62e71d6b4bfd160eb0
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2021-06-24 21:46:06 +0200

    Trying typescript application Fix errors later

---
+++ tsapp/my-app/src/components/menu/Menu.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./menu.css";
+
+export default function Menu({menuOpen, setMenuOpen}) {
+	return (
+		<div className={"menu " + (menuOpen && "active")} id="menu">
+			<ul>
+				{/* TODO: Create component of listelement so that the setMenuOpen() is only used once. */}
+				<li onClick={()=>setMenuOpen(false)}>
+					<a href="#intro">Home</a>
+				</li>
+				<li onClick={()=>setMenuOpen(false)}>
+					<a href="#portfolio">Portfolio</a>
+				</li>
+				<li onClick={()=>setMenuOpen(false)}>
+					<a href="#works">Works</a>
+				</li>
+				<li onClick={()=>setMenuOpen(false)}>
+					<a href="#testimonials">Testimonials</a>
+				</li>
+				<li onClick={()=>setMenuOpen(false)}>
+					<a href="#contact">Contact</a>
+				</li>
+			</ul>
+		</div>
+	)
+}
