8b56f0cff1fd1816649216ed9aef72d045338a84
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2021-07-01 13:08:56 +0200

    Removed wait time

--- app/cypress/integration/navigation.spec.js
+++ app/cypress/integration/navigation.spec.js
@@ -3,24 +3,23 @@ describe('Site navigation', () => {
 		cy.visit('http://localhost:3000');
 	});
 	it('Navigates to Home', () => {
-		cy.wait(2000);
 		cy.navTo('Home', '/#intro');
+		cy.get('h1').contains('Andreas Petersen');
 	});
 	it('Navigates to Portfolio', () => {
-		cy.wait(2000);
 		cy.navTo('Portfolio', '/#portfolio');
+		cy.get('h1').contains('Portfolio');
 	});
 	it('Navigates to Works', () => {
-		
-		cy.wait(2000);
 		cy.navTo('Works', '/#works');
+		cy.get('span').contains('Projects');
 	});
 	it('Navigates to Testimonials', () => {
-		cy.wait(2000);
 		cy.navTo('Testimonials', '/#testimonials');
+		cy.get('h1').contains('Testimonials');
 	});
 	it('Navigates to Contact', () => {
-		cy.wait(2000);
 		cy.navTo('Contact', '/#contact');
+		cy.get('h2').contains('Contact');
 	});
 });
