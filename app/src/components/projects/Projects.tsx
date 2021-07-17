f8aa5c575a07f93f67f06a88f9f17862779cc5fe
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2021-07-17 10:47:01 +0200

    Addedd component

---
+++ app/src/components/projects/Projects.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useRef } from 'react';
+import { init } from 'ityped';
+import './projects.scss';
+
+export default function Projects() {
+	
+	return (
+		<div className="projects" id="projects">
+		</div>
+	);
+}
