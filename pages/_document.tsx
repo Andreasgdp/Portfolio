2a00355adb36a303fafc9b23fa87b3197de4f1e2
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-11-04 21:25:11 +0100

    Fix chrome lighthouse pwa test (passes now)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,6 +24,20 @@ export default class Document extends NextDocument {
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
         />
 
+        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/icon-152x152.png"
+        />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/logo.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&display=swap"
+        />
+
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
