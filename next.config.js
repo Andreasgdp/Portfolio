b8788e8dbd384e3a4ed375cf21da851e75913f60
Andreas Petersen (Guldberg) <andreasgdp@gmail.com>
2022-11-04 21:05:48 +0100

    Add PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const withPWA = require('next-pwa')({
+  dest: 'public'
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -13,4 +17,4 @@ const nextConfig = {
   }
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import { ColorModeScript } from '@chakra-ui/react';
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
 
 import theme from '../libs/theme';
 
@@ -8,6 +8,22 @@ export default class Document extends NextDocument {
     return (
       <Html lang="en">
         <Head />
+        <meta name="Guldberg Portfolio" content="PWA App" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PWA App" />
+        <meta name="description" content="Best PWA App in the world" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#2B5797" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
