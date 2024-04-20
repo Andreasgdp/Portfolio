23a15a512f58ce18c69f35026010f521bd93df59
Andreas Petersen <andreasgdp@gmail.com>
2024-04-20 20:40:31 +0200

    Use git to get more correct date for edit for docs

--- apps/web/util/utils.ts
+++ apps/web/util/utils.ts
@@ -1,22 +1,30 @@
-import type { DocumentGen } from 'contentlayer/core'
-import * as fs from 'node:fs/promises'
-import path from 'node:path'
+import type { DocumentGen } from "contentlayer/core";
+import path from "node:path";
+import { simpleGit, SimpleGit, CleanOptions } from "simple-git";
 
-export const contentDirPath = 'content'
+export const contentDirPath = "content";
 
 export const urlFromFilePath = (doc: DocumentGen): string => {
-  let urlPath = doc._raw.flattenedPath.replace(/^pages\/?/, '/')
-  if (!urlPath.startsWith('/')) urlPath = `/${urlPath}`
-  if ('global_id' in doc) urlPath += `-${doc.global_id}`
+  let urlPath = doc._raw.flattenedPath.replace(/^pages\/?/, "/");
+  if (!urlPath.startsWith("/")) urlPath = `/${urlPath}`;
+  if ("global_id" in doc) urlPath += `-${doc.global_id}`;
+
   // Remove preceding indexes from path segments
   urlPath = urlPath
-    .split('/')
-    .map((segment) => segment.replace(/^\d\d\d\-/, ''))
-    .join('/')
-  return urlPath
-}
+    .split("/")
+    .map((segment) => segment.replace(/^\d\d\d\-/, ""))
+    .join("/");
+  return urlPath;
+};
 
 export const getLastEditedDate = async (doc: DocumentGen): Promise<Date> => {
-  const stats = await fs.stat(path.join(contentDirPath, doc._raw.sourceFilePath))
-  return stats.mtime
-}
+  const sourceFilePath = path.join(contentDirPath, doc._raw.sourceFilePath);
+  const git: SimpleGit = simpleGit().clean(CleanOptions.FORCE);
+  const lastCommit = await git.log({ file: sourceFilePath });
+
+  if (lastCommit.latest === null) {
+    return new Date();
+  }
+
+  return new Date(lastCommit.latest.date);
+};
